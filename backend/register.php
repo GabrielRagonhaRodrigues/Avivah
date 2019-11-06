d860f6725f86c3a7f623d8d6ff62e065af078c7b
Gabriel <gabriel.rodrigues313@etec.sp.gov.br>
2019-11-06 11:22:11 -0200

    Tela de Registro Avivah

---
+++ backend/register.php
@@ -0,0 +1,25 @@
+<?php
+
+include_once 'conexao.php';
+
+$name =
+$sexo =
+$estadocivil =
+$datanasc =
+$nacionality =
+$cep =
+$state =
+$city =
+$district =
+$numero =
+$complement =
+$cpf =
+$telephone1 =
+$telephone2 =
+$cel1 =
+$cel2 =
+$email1 =
+$email2 =
+$user = $_POST['user'];
+$pass = $_POST['password'];
+?>
