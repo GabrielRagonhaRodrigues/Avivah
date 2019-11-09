85a94c7497295ee7300803c8f05aca3d227ef428
Gabriel Ragonha Rodrigues <gabriel.rodrigues313@etec.sp.gov.br>
2019-11-08 21:30:51 -0300

    Autenticação do login

--- backend/auth.php
+++ backend/auth.php
@@ -5,8 +5,18 @@ include_once 'conexao.php';
 $user = $_POST['user'];
 $pass = $_POST['password'];
 
-$query = "SELECT * FROM login WHERE username='" . $_POST["user"] . "' and password = '". $_POST["password"]."'";
+$query = "SELECT * FROM funcionarios WHERE username='" . $_POST["user"] . "' and password = '". $_POST["password"]."'";
 
+if(!$query){
+    
+    $query = "SELECT * FROM fornecedores WHERE username='" . $_POST["user"] . "' and password = '". $_POST["password"]."'"; 
+    
+    if(!$query == false){
+        
+        $query = "SELECT * FROM users WHERE username='" . $_POST["user"] . "' and password = '". $_POST["password"]."'";
+        }
+    }
+    
 $result = mysqli_query($conexao, $query);
 $data = (mysqli_fetch_row ($result));
 
