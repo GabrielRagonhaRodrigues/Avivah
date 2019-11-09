6845dacd4bd3a085e893fec8aa5a99c6613ae7f8
Gabriel Ragonha Rodrigues <gabriel.rodrigues313@etec.sp.gov.br>
2019-11-08 23:14:47 -0300

    Conexão do banco

--- backend/conexao.php
+++ backend/conexao.php
@@ -4,10 +4,16 @@ $host = "localhost";
 $user = "root";
 $pass = "";
 $banco = "avivah";
+$port = 3308;
 
 //Cria a conexão
-$conexao = new mysqli ($host, $user,$pass) or die ("Não encontrado servidor");
+$conexao = new mysqli ($host, $user,$pass,$banco,$port) or die ("Não encontrado servidor");
 
+//Mostra o erro caso seja dado
+if ($conexao->connect_error) {
+    die('Connect Error (' . $conexao->connect_error . ') '
+            . $conexao->connect_error);
+}
 //Checa a conexão
 $acesso = $conexao->select_db($banco) or die ("Não foi localizado o banco");
 //header("Location:../frontend/index.php");
